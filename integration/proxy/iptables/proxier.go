9ef3034aef39f8b2c4367fcb0516b033e77cfd9e
Fabiano Fidêncio <fidencio@redhat.com>
2020-09-04 00:09:32 +0200

    Stop relying on vendored features
    Signed-off-by: Fabiano Fidêncio <fidencio@redhat.com>

--- integration/proxy/iptables/proxier.go
+++ integration/proxy/iptables/proxier.go
@@ -33,6 +33,7 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/cri-o/cri-o/integration/features"
 	v1 "k8s.io/api/core/v1"
 	discovery "k8s.io/api/discovery/v1beta1"
 	"k8s.io/apimachinery/pkg/types"
@@ -40,7 +41,6 @@ import (
 	utilfeature "k8s.io/apiserver/pkg/util/feature"
 	"k8s.io/client-go/tools/record"
 	"k8s.io/klog/v2"
-	"k8s.io/kubernetes/pkg/features"
 	"k8s.io/kubernetes/pkg/proxy"
 	"k8s.io/kubernetes/pkg/proxy/healthcheck"
 	"k8s.io/kubernetes/pkg/proxy/metaproxier"
